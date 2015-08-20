e134433d69c37ed839be683783989e794bf718ca
PieterVanPittens <pieter.van.pittens@outlook.com>
2015-08-20 21:44:46 +0200

    pw wrong

--- test.php
+++ test.php
@@ -10,7 +10,7 @@ class MoneyTest extends PHPUnit_Framework_TestCase {
     {
 	$servername = "127.0.0.1";
 $username = "root";
-$password = "";
+$password = "xcx";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
