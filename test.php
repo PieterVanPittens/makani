f10ff6034be9c7f7601423f8b28a9cbe8f1859dd
PieterVanPittens <pieter.van.pittens@outlook.com>
2015-08-20 21:43:43 +0200

    mysql test

--- test.php
+++ test.php
@@ -5,5 +5,29 @@ class MoneyTest extends PHPUnit_Framework_TestCase {
     {
         $this->assertEquals(true, true);
     }
+	
+  public function testCanConnectMySql()
+    {
+	$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+$isConnected = false;
+} else {
+$isConnected = true;
+}
+
+
+	$this->assertEquals(true, $isConnected);
+    }
+	
+	
+	
+	
 }
 ?>
