8490852686f17b5d5bb39f8ef3d87501d7b2ec4f
PieterVanPittens <pieter.van.pittens@outlook.com>
2015-08-20 21:38:57 +0200

    true

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ class MoneyTest extends PHPUnit_Framework_TestCase {
 
   public function testObjectCanBeConstructedFromStringValue()
     {
-        $this->assertEquals(true, false);
+        $this->assertEquals(true, true);
     }
 }
 ?>
